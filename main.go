9bda0d63e1162e4f2e35a7fa9a19533efc97a228
yanshen1997 <yans@mail.ustc.edu.cn>
2022-03-23 15:05:00 +0800

    feat

--- main.go
+++ main.go
@@ -7,23 +7,22 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/yanshen1997/simplebank/api"
 	db "github.com/yanshen1997/simplebank/db/sqlc"
-)
-
-const (
-	sqlDriver     = "postgres"
-	sqlSourceName = "postgres://root:123456@localhost:5432/simple_bank?sslmode=disable"
-	address       = "0.0.0.0:8080"
+	"github.com/yanshen1997/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(sqlDriver, sqlSourceName)
+	config, err := util.Load(".")
+	if err != nil {
+		log.Fatal("can not load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("can not open db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(*store)
 
-	if err = server.Start(address); err != nil {
+	if err = server.Start(config.ServerAddress); err != nil {
 		log.Fatal("can not start server:", err)
 	}
 }
---
+++ util/config.go
@@ -0,0 +1,23 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func Load(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
